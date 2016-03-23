b3a6db17bc5f72a30d1fefca3e2b7135dc6bde72
Trung Tran <lonesometrung@yahoo.com>
2016-03-23 15:07:15 -0400

    Cheese

---
+++ TrungPetStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrungPetStore.Startup))]
+namespace TrungPetStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
